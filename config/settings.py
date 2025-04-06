f9ffcaedf68c37bae139c8f57aaa612bb168f6f2
ArtemUbuntu <Danchenko.A@edu.rea.ru>
2025-04-06 12:33:45 +0300

    Добавлены переменные окружения и обновлен settings.py

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -20,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&cfp-dk99oclx4_@drjrii%cjzd##=##%35y9%7w78j$y()j*@'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
